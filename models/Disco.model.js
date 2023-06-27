bd334d099350dd8cb96127dfa9a6ac34f225cf28
Javier Vivas <javier.vivas@icloud.com>
2023-06-27 19:50:38 +0200

    Create disco model and routes

--- models/Disco.model.js
+++ models/Disco.model.js
@@ -3,21 +3,12 @@ const { Schema, model } = require("mongoose");
 const DiscoSchema = new Schema({
   name: String,
   address: String,
-  postalCode: String,
   town: String,
   province: String,
   community: String,
-  phone: String,
   web: String,
   latitude: String,
   longitude: String,
-  activity: String,
-  references: String,
-  principalActivity: String,
-  companyType: String,
-  facebook: String,
-  idFromAPI: String,
-  google_maps: String,
 });
 
 module.exports = model("Disco", DiscoSchema);
