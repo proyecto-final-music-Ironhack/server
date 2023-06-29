711d10defe29873f15a4df940d3521ee99d88f41
Javier Vivas <javier.vivas@icloud.com>
2023-06-29 20:54:04 +0200

    auth OK

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 // const authRoutes = require("./routes/auth.routes");
 // app.use("/auth", authRoutes);
-const authRoutes = require("./routes/authnew.routes");
+const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 const userRoutes = require("./routes/user.routes");
-app.use("/user", userRoutes )
+app.use("/users", userRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 router.use("/disco", require("./disco.routes"));
 router.use("/auth", require("./auth.routes"));
 router.use("/event", require("./event.routes"));
-
+router.use("/users", require("./user.routes"));
 
 module.exports = router;
