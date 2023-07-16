7c376da17a47e3a8ebebfc8f48f089a58a524b7d
Javier Vivas <javier.vivas@icloud.com>
2023-07-16 17:11:34 +0200

    Route index of upload

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,4 +9,5 @@ router.use("/users", isAuthenticated, require("./user.routes"));
 router.use("/djs", isAuthenticated, require("./dj.routes"));
 router.use("/reviews", isAuthenticated, require("./review.routes"));
 router.use("/spotify", isAuthenticated, require("./spotify.routes"));
+router.use("/upload", require("./upload.routes"));
 module.exports = router;
